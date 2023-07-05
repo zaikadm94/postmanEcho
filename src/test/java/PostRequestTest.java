d2866260a801ebf480132cbb0f94c98f47678595
zaikadm94 <zaikadm94@mail.ru>
2023-07-05 13:56:50 +0300

    incorrect test

--- src/test/java/PostRequestTest.java
+++ src/test/java/PostRequestTest.java
@@ -17,7 +17,7 @@ public class PostRequestTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some data"))
+                .body("data",equalTo("some duiui"))
         ;
     }
 }
